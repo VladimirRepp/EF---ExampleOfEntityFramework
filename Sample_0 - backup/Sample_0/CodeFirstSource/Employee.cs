c015d9da833c1b5df5fedb9260ae4184ee7d6afa
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-01-29 15:34:21 +0700

    Add files via upload

---
+++ Sample_0 - backup/Sample_0/CodeFirstSource/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_0.CodeFirstSource
+{
+    public class Employee
+    {
+        public int Id       { get; set; } // Поле Id или NameClassId обязательно 
+        public string Name  { get; set; }
+        public int Age      { get; set; }
+    }
+}
